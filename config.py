cdecfc58eaacc03b016500d21b992c3abac4727b
Gorienich <107270919+Gorienich@users.noreply.github.com>
2024-07-18 00:33:38 +0300

    app.py config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-
+# main config
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://username:password@db:5432/openai_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
